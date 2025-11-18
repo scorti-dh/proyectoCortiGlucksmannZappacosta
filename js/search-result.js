40862ed640f71b491bf66f5a70ee1e30a9dedc91
Simongluck <sglucksmann@udesa.edu.ar>
2025-11-18 12:46:31 -0300

    El search results

--- js/search-result.js
+++ js/search-result.js
@@ -1,10 +1,72 @@
 //resultados de busqueda 
+let urlCat = 'https://dummyjson.com/products/categories'
 
-let a = 'https://dummyjson.com/products'
+let aside = document.querySelector(".ul_aside")
 
-fetch(a)
-
-.then(function (res) {
+fetch(urlCat)
+    .then(function (res) {
         return res.json()
     })
-.then
+
+    .then(function (data) {
+        console.log(data)
+        for (let i = 0; i < data.length; i++) {
+            aside.innerHTML +=
+                <li> <a href="./category.html?name=${data[i].name}">${data[i].name}</a></li>
+        }
+    })
+
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+
+let searchName = queryStringObj.get('Buscar');
+console.log(searchName);
+
+let urlNueva = 'https://dummyjson.com/products/search?q=' + searchName;
+
+console.log(urlNueva);
+
+fetch(urlNueva)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        let productos = data.products;
+        let titulo = document.querySelector('#search_main');
+        console.log(productos);
+
+        titulo.innerHTML += ` 
+            <h2 class="titulos">Results for: ${searchName}</h2>
+            <section class="secciones vendidos"></section>
+        `;
+
+        let vendidos = document.querySelector('.vendidos');
+
+        for (let i = 0; i < productos.length; i++) {
+            vendidos.innerHTML += ` 
+                <article class="article_category">
+                    <img src="${productos[i].thumbnail}">
+                    <h3 class="h3_productos">${productos[i].title}</h3>
+                    <p>${productos[i].description}</p>
+                    <p>${productos[i].price}</p>
+                    <a class="boton" href="product.html?id=${productos[i].id}">Ver Detalle</a>
+                    <section class="section_ssr">
+                        <div class="div_ssr">
+                            <h4>Stock</h4>
+                            <p class="div_p ssr_p">${productos[i].stock}</p>
+                        </div> 
+                        <div class="div_ssr">
+                            <h4>Rating</h4>
+                            <p class="div_p ssr_p">${productos[i].rating}</p> 
+                        </div>
+                        <div class="div_ssr">
+                            <h4>Brand:</h4>
+                            <p class="div_p ssr_p">${productos[i].brand}</p> 
+                        </div>
+                    </section>
+                </article>
+            `;
+        }
+    });
+
+    
